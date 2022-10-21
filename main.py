c368d21a240ed053dbff49ab2da34882f1d56c0f
Campioni Lorenzo (IFL ATV SC D RAD PJM) <Lorenzo.Campioni@infineon.com>
2022-10-21 16:27:29 +0200

    Working version

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 import requests
 import json
-import weatherFinder
-
-
-SunSeeker = weatherFinder()
+from weatherFinder import weatherFinder
 
 
 
+def SeekSunnyStations() :
 
-response = requests.get(SunSeeker.DBGendpoint+"/stations", params={'query' : SunSeeker.stationName})
-
-#first output of the query will be taken as departure station
-departure_station = response.json()[0]
-
-#I check first the weather at the departure station
-WeatherAtDeparture = SunSeeker.getWeatherAtStation(departure_station)
-if (WeatherAtDeparture) :
-    #parameter to be selected here, I think I will choose cloudiness
-    pass
-
-data = requests.get(SunSeeker.DBGendpoint+"/"+departure_station['id'])
+    SunSeeker = weatherFinder()
 
 
-# I will get here an array of reachable train station by direct train connection
-# they are already sorted with increasing departure
-# I need to check the weather for each of these points until I find one that satisfy the requirements (ex: cloudiness < 10%)
-#for loop WeatherAtLocation = SunSeeker.getWeatherAtStation(departure_station)
+    response = requests.get(SunSeeker.DBGendpoint+"/stations", params={'query' : SunSeeker.stationName})
 
-#######
+    #first output of the query will be taken as departure station
+    departure_station = response.json()[0]
 
-# I need to check if this location is in Austria 
-station = requests.get(SunSeeker.DBGendpoint+"/stations"+departure_station['id'])
-# I can eventually check if the country is Austria (I could also do this upfront for all the stations)
-country = station['country']
+    #I check first the weather at the departure station
+    # if (SunSeeker.checkWeatherAtStation(departure_station)) :
+        # return (departure_station)
 
-# I can then stop and:
-# 
-# provide the result with the name of the station and the weather
-# eventually check the next departing train (dep to destination) 
+
+
+    if (SunSeeker.config['klimaticket'] and SunSeeker.checkStationCountry(departure_station)) : 
+        print ("Departure station not in Austria, but klimaticket active")
+        return -1
+    data = requests.get(SunSeeker.DBGendpoint+"/"+departure_station['id']).json()
+
+    sunny_stations = []
+    # I will get here an array of reachable train station by direct train connection
+    # they are already sorted with increasing departure
+    # I need to check the weather for each of these points until I find one that satisfy the requirements (ex: cloudiness < 10%)
+    #for loop 
+    for station in data :
+        if (SunSeeker.config['klimaticket'] and SunSeeker.checkStationCountry(departure_station)) : 
+            print ("Station outside Austria, skipping it.")
+            continue
+        (sunnyThere, forecast) = SunSeeker.checkWeatherAtStation(station)
+        if (sunnyThere) :
+            station['forecast'] = forecast
+            sunny_stations.append(station)
+        if (len(sunny_stations) > 5 ) : break
+    #######
+    return sunny_stations
+    # I can then stop and:
+    # 
+    # provide the result with the name of the station and the weather
+    # eventually check the next departing train (dep to destination) 
+    
+
+print(SeekSunnyStations())
--- weatherFinder.py
+++ weatherFinder.py
@@ -1,9 +1,10 @@
 from pyowm.owm import OWM
 import json
+import requests
 
 class weatherFinder :
 
-    def __init__(self) -> None:
+    def __init__(self) :
         self.DBGendpoint = 'https://api.direkt.bahn.guru'
 
         with open('config.json', 'r') as f:
@@ -11,9 +12,8 @@ class weatherFinder :
 
 
         self.stationName = self.config['station_name']
-        self.owmApiKey = self.config['owm_api_key']
 
-        self.owm = OWM('missing-api-key')
+        self.owm = OWM(self.config['owm_api_key'])
         self.owmMgr = self.owm.weather_manager()
 
 
@@ -21,3 +21,21 @@ class weatherFinder :
     def getWeatherAtStation (self, station):
         return self.owmMgr.one_call(lat=station['location']['latitude'], lon=station['location']['longitude'])
     
+
+    def checkWeatherAtStation (self, station) :
+        Weather = self.getWeatherAtStation(station)
+
+        forecast = getattr(Weather, self.config['owm_forecast'])
+        status = getattr(forecast, self.config['owm_decision_parameter'])
+
+        if (status <=  self.config['owm_decision_threshold']) :
+            #parameter to be selected here, I think I will choose cloudiness
+            return (1, forecast)  #sun has been found
+        else : 
+            return (0, forecast) # no sun
+    def checkStationCountry(self, station) :
+       
+        station = requests.get(self.DBGendpoint+"/stations/"+station['id']).json()
+         # I can eventually check if the country is Austria (I could also do this upfront for all the stations)
+        if (station['country'] != 'AT') : return -1
+        else : return 0
